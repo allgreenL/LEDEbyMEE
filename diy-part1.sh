faec22f887faf481b3ab3b912734bbd328248943
allgreenL <66295940+allgreenL@users.noreply.github.com>
2020-09-14 09:27:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,12 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
+cd package/lean/
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+cd ../../
+
+
+
